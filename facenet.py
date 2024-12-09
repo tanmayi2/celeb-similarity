4f7777103a6e01c385ffea9251f57ea0a3e74554
tanmayi2 <tanmayi.dasari@gmail.com>
2024-12-09 00:17:50 +0000

    attempting to use celeba dataset

--- facenet.py
+++ facenet.py
@@ -1,13 +1,66 @@
 from facenet_pytorch import MTCNN, InceptionResnetV1
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets
 import numpy as np
 import pandas as pd
+from torchvision import transforms
+from PIL import Image
 import os
 
 workers = 0 if os.name == 'nt' else 4
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+# preprocessing
+transform = transforms.Compose([
+    transforms.Resize((160, 160)), #might want to remove this
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+])
+
+
+class CelebADataset(Dataset):
+    def __init__(self, img_dir, identity_file, bbox_file, transform=None):
+        self.img_dir = img_dir
+        self.transform = transform
+        self.data = pd.read_csv(identity_file, sep=' ', header=None, names=['image', 'identity'])
+        self.image_paths = self.data['image'].values
+        self.identities = self.data['identity'].values
+        self.bbox_data = pd.read_csv(bbox_file)
+
+        self.bbox_dict = {
+            row['image_id']: (row['x_1'], row['y_1'], row['width'], row['height'])
+            for _, row in self.bbox_data.iterrows()
+        }
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.image_paths[idx])
+        image = Image.open(img_path).convert('RGB')
+        identity = self.identities[idx]
+
+        #bounding box
+        bbox = self.bbox_dict.get(self.image_paths[idx])
+        if bbox:
+            x, y, w, h = bbox
+            image = image.crop((x, y, x + w, y + h))
+
+        if self.transform:
+            image = self.transform(image)
+        return image, identity
+
+#dataloader
+project_root = os.path.dirname(__file__) 
+
+#put celeba dataset under data directory
+img_dir = os.path.join(project_root, 'data', 'celeba', 'img_align_celeba')
+identity_file = os.path.join(project_root, 'data', 'celeba', 'identity_CelebA.txt')
+bbox_file = os.path.join(project_root, 'data', 'celeba', 'list_bbox_celeba.csv')
+dataset = CelebADataset(img_dir, identity_file, bbox_file, transform=transform)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=False, num_workers=workers)
 
 mtcnn = MTCNN(
     image_size=160, margin=0, min_face_size=20,
@@ -20,31 +73,29 @@ resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
 def collate_fn(x):
     return x[0]
 
-dataset = datasets.ImageFolder('data/test_images')
-dataset.idx_to_class = {i:c for c, i in dataset.class_to_idx.items()}
-loader = DataLoader(dataset, collate_fn=collate_fn, num_workers=workers)
 
 aligned = []
-names = []
-for x, y in loader:
-    x_aligned, prob = mtcnn(x, return_prob=True)
-    # print(x_aligned)
+ids = []
+for images, identities in dataloader:
+    images = images.to(device)
+    x_aligned, probs = mtcnn(images, return_prob=True)
     if x_aligned is not None:
-        print('Face detected with probability: {:8f}'.format(prob))
-        aligned.append(x_aligned)
-        names.append(dataset.idx_to_class[y])
-
+        for aligned_img, prob, identity in zip(x_aligned, probs, identities):
+            if prob is not None and prob > 0.9:  # Confidence threshold
+                aligned.append(aligned_img)
+                ids.append(identity.item())
 
 aligned = torch.stack(aligned).to(device)
 embeddings = resnet(aligned).detach().cpu()
 
-# create dataframe with the names and embeddings
-df = pd.DataFrame({"name": names, "embedding": list(embeddings)})
+# create dataframe with the identities and embeddings
+df = pd.DataFrame({"identity": ids, "embedding": list(embeddings)})
 
-# Group by 'name' and then average the tensors for the same person
+# group by identity and average embeddings
 def average_embeddings(embeddings):
     return np.mean(np.vstack(embeddings), axis=0)
-average_df = df.groupby('name')['embedding'].agg(average_embeddings)
+
+average_df = df.groupby('identity')['embedding'].agg(average_embeddings)
 
 #create distribution chart
 dists = [[torch.tensor(e1 - e2).norm().item() for e2 in average_df] for e1 in average_df]
